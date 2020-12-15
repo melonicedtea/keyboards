4c7c50093d5555a0a52696fd8508f24d3f418353
melonicedtea <mickycheng16@hotmail.com>
2020-12-15 17:18:35 +0100

    Changed files

--- anne_pro/config.h
+++ anne_pro/config.h
@@ -42,7 +42,7 @@
  * Timeout after which the backlight of the keyboard is disabled if no keypresses are received.
  * If this feature is not needed you can set the timeout to UINT32_MAX, this is more than 100 years.
  */
-#define BACKLIGHT_TIMEOUT 60
+#define BACKLIGHT_TIMEOUT 1800
 
 /*
  * Force NKRO
